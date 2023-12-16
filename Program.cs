049dd51d32ad35dcc0ccfa9c594f0396306a507e
Kenneth Stanley <kstanley_bass@hotmail.com>
2023-12-16 11:33:25 -0300

    [Programa.cs]
    Trabalhando com data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿using Fundamentos_Csharp.Models;
+﻿using System.Data;
+using Fundamentos_Csharp.Models;
 
-Pessoa p1 = new Pessoa();
 
-p1.nome = " Kenneth";
-p1.idade = 41;
-p1.Apresentar();
+DateTime dataAtual = DateTime.Now;
+
+Console.WriteLine(dataAtual);
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.nome = " Kenneth";
+// p1.idade = 41;
+// p1.Apresentar();
+
